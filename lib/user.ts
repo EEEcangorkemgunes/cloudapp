a383e1acd4ef71ae626ad3228c61529cda27abdc
can <eee.gunescangorkem@gmail.com>
2024-07-08 17:18:25 +0300

    revalidate path for users page

--- lib/user.ts
+++ lib/user.ts
@@ -1,9 +1,12 @@
+"use server";
 import { users } from "@/data/user";
+import { revalidatePath } from "next/cache";
 
 export const findUserByEmail = async (email: string) => {
   return users.find((user) => user.email === email);
 };
 
 export const getAllUsers = async () => {
+  revalidatePath("/users");
   return users;
 };
