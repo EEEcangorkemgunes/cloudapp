8c1fc47e05494a007b0e73c580687a0cbc29047f
Cihan Senyuz <121491287+cihansenyuz@users.noreply.github.com>
2024-07-17 09:39:44 +0300

    Update route.ts
    fix content-type of GET request where status == 200

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -40,6 +40,7 @@ export async function GET(req: NextRequest) {
     return new Response(JSON.stringify(data), {
       status: 200,
       headers: {
+        "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
